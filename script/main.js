8dbe2251b872ae0b48b7bd39b8930d0438eec34b
Fabio Gadau <fabiogadau92@gmail.com>
2020-05-04 11:52:28 +0200

    first commit

---
+++ script/main.js
@@ -0,0 +1,21 @@
+/*
+* BOOLFLIX
+
+- Milestone 1: 
+Creare un layout base con una searchbar (una input e un button) in cui possiamo 
+scrivere completamente o parzialmente il nome di un film. Possiamo, cliccando il 
+bottone, cercare sull’API tutti i film che contengono ciò che ha scritto l’utente. 
+Vogliamo dopo la risposta dell’API visualizzare a schermo i seguenti valori per ogni 
+film trovato:  
+1. Titolo 
+2. Titolo Originale 
+3. Lingua Originale 
+4. Voto (media) 
+Utilizzare un template Handlebars per mostrare ogni singolo film trovato. 
+*/
+
+$(document).ready(function() {
+
+  
+
+});
